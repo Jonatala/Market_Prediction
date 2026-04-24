5c16bcbcc45265580821a3503ed0c27b22d3706b
JonA <olaatala7@gmail.com>
2026-04-24 13:41:26 +0100

    FastApi added to project

--- src/api/app.py
+++ src/api/app.py
@@ -5,3 +5,18 @@ import os
 
 app = FastAPI()
 
+MODEL_PATH = "../../models/randon_forest_model.pkl"
+
+@app.get("/")
+def home():
+    return {"message": "Market Prediction API Running"}
+
+@app.post("/predict")
+def predict(data: dict):
+    """Expect JSON input with feature values"""
+
+    df = pd.DataFrame([data])
+
+    prediction = model.predict(df)[0]
+
+    return {"prediction": float(prediction)}
